922f72d0561b1a41c5b25e4bea9498bd3386b985
Shayan <nwryshayan500@gmail.com>
2025-03-15 12:23:56 +0330

    014 Nested Routes Inside Dynamic Routes

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 import { DUMMY_NEWS } from "@/dummy-news";
@@ -5,11 +6,15 @@ import { DUMMY_NEWS } from "@/dummy-news";
 export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug)
+
   if(!newsItem) notFound()
+
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsSlug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
