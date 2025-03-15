5ffdab424100ebda4d0287c5020de5b693f5125e
Shayan <nwryshayan500@gmail.com>
2025-03-15 12:48:57 +0330

    015 Intercepting Navigation & Using Interception Routes

--- app/news/[slug]/(.)image/page.js
+++ app/news/[slug]/(.)image/page.js
@@ -2,15 +2,18 @@ import { notFound } from "next/navigation";
 
 import { DUMMY_NEWS } from "@/dummy-news";
 
-export default function ImagePage({ params }) {
+export default function InterceptedImagePage({ params }) {
   const newsSlug = params.slug
   const newsItem = DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug) 
 
   if(!newsItem) notFound()
   
   return (
-    <div className="fullscreen-image">
-      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-    </div>
+    <>
+      <h1>Intercepted!</h1>
+      <div className="fullscreen-image">
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+      </div>
+    </>
   );
 }
